bf792c24ddcf88da49413b610c029b6db0d6b58b
charlie <youmax210139@gmail.com>
2022-08-18 12:10:50 +0800

    feat: admin table/guard

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -9,7 +9,7 @@ use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Facades\Hash;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class Admin extends Authenticatable
 {
     use HasApiTokens;
     use HasFactory;
